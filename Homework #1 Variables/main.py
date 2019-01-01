44000b5777194e9ed63f453d1d286fbaa81dbf4c
Caleb Hillary <calebhillary97@gmail.com>
2019-01-01 22:32:14 +0530

    Completed Homework #1: Variables. Added details of song: Alone - By Alan Walker

---
+++ Homework #1 Variables/main.py
@@ -0,0 +1,78 @@
+# Homework #1: Variables
+
+"""
+    Homework Assignment submitted to Python Is Easy course at Pirple
+    Homework #1: Variables
+    Written by Caleb Hillary on 01 January, 2019
+
+    Below are the details of a song I like: Alone - by Alan Walker
+"""
+# Name of the song
+Name = "Alone"
+
+# Name of the artist/producer of the song
+Artist = "Alan Walker"
+
+# Album the song is found in. Album contains two other songs
+Album = "Different World"
+
+# Year of release
+Year = 2016
+
+# Date of release
+ReleaseDate = "2nd DEC 2016"
+
+# Genre of song. Song can be classified as Pop and EDM
+Genre = "EDM/Pop"
+
+# Duration of song(Doesn't include extra few seconds found in music video)
+DurationInSeconds = 163
+DurationInMinutes = 2.68
+
+# A random value added as song was released in "digital download" format
+CopiesSold = 17828734
+
+# Label under which song was released
+Label = "Mer Musikk"
+
+# Vocal artist(unofficial)
+Vocals = "Noonie Bao"
+
+# Links - 
+OfficialLink = "http://alone.alanwalker.no/"
+YoutubeLink = "https://www.youtube.com/watch?v=1-xGerv5FOk"
+SpotifyLink = "https://open.spotify.com/album/7kNg4r6nqFHGBj3XxJLi4K"
+
+# Music video details
+CurrentViewsOnYoutube = 796821434
+MusicVideoFilmingLocation = "Bergen, Norway"
+
+# Other vesions
+AcousticVersion = "Alone (Restrung)"
+
+# Awards
+Nominations = "Spellemann Award for Hit of the Year"
+Awards = "Grammis Award for Composer of the Year"
+
+
+# Printing the song details
+print(Name)
+print(Artist)
+print(Album)
+print(Year)
+print(Genre)
+print(DurationInSeconds)
+print(DurationInMinutes)
+print(CopiesSold)
+print(Label)
+print(Vocals)
+print(OfficialLink)
+print(YoutubeLink)
+print(CurrentViewsOnYoutube)
+print(SpotifyLink)
+print(AcousticVersion)
+print(MusicVideoFilmingLocation)
+print(Nominations)
+print(Awards)
+
+#End of file
