4dcd92e84f0af3797ce6ec36ecb1037222ba6c02
Caleb Hillary <calebhillary97@gmail.com>
2019-01-03 01:29:48 +0530

    Homework assignment submitted to Python is Easy course at pirple - Caleb Hillary

---
+++ Homework Assignment #2 Functions/Assignment2.py
@@ -0,0 +1,28 @@
+# Homework Assignment #2: Functions
+
+"""
+    Homework Assignment submitted to Python Is Easy course at Pirple
+    Homework Assignment #2: Functions
+    Written by Caleb Hillary on 03 January, 2019
+
+    Below are functions that return details of a song I like: Alone - by Alan Walker
+"""
+
+# Returns release year of song
+def year():
+    return 2016
+
+# Vocals for the song were done by Noonie Bao(unofficial)
+def vocals():
+    return "Noonie Bao"
+
+# Function returns youtube link
+def youtubeLink():
+    return "https://www.youtube.com/watch?v=1-xGerv5FOk"
+
+
+# Current views on youtube is 796,821,434. If views is greater than 5,000,000 function returns true
+def isYoutubeViewsMoreThanFiveMillion():
+    return True
+
+#End of file
